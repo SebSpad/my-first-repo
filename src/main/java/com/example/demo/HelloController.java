9524156aa39ff3a8ad84bed5cb61e1c8e92af82a
SebSpad <35527790+SebSpad@users.noreply.github.com>
2025-07-29 15:23:06 +0200

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World!";
+        return "Hello World ! Here is my firs Github Actions Pipeline ...";
     }
 }
